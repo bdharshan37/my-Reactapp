c1ee4fe954979153ca2c1ad9dbc9b121f7b2c9e4
bdharshan37 <dharshan37@outlook.com>
2020-04-25 23:05:43 +0530

    Text change

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hey this is my new portfolio</h1>
+    <p>I am a User Experience designer</p>
+    <p>With 3 years of experience</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Check My Projects</Link>
+  </Layout>
+)
+
+export default IndexPage
