415b5891ae6e025e61c7e9b22796d8326b50e8a4
bdharshan37 <dharshan37@outlook.com>
2020-04-28 23:52:11 +0530

    Title animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,9 @@ const IndexPage = () => (
     <Header />       
     <div className="Hero">
       <div className="HeroGroup">
-      <h1>Hye Hooman</h1>
-      <p>I am a Interaction designer + a curious hooman being With 3 years of experience in [code] + [design]   </p>
-  
+      <h1>Hye Hooman <br/> ......</h1>
+      <p>I am a Interaction designer<br /> named as Baladharshan ;-p <br />considered to be a curious hooman being <br />  With 3 years of experience in <br /> [code] + [design] iN    2020  </p>
+  <br />
       <Link to="/page-2/">Check My Projects</Link>
       </div>
     </div>
